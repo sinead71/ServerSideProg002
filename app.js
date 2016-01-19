ed8abcc5937330a3acd23cd18b0f13a4c27ffcd8
sinead.71 <cooneysinead71.com>
2016-01-19 15:17:38 +0000

    making variables in js

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log ("Hello World");
+
+function add(a, b){
+	return a+b;
+}
+
+var theAns = add(5, 7);
+console.log(theAns);
+
+//declaring variables
+var name = "Sinead";//float
+var age = 21; //int
+var female = true; //boolean
+
+if (age == 21){
+
+	console.log("you're still young");
+}
+
+var o = new Object(); //similar to action script
+
+var myArray = new Array();
+
+var anotherObject = {};
