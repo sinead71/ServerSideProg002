362910a63e9a61bb508f485ad569cec4bd1649ab
sinead.71 <cooneysinead71.com>
2016-01-19 15:38:42 +0000

    more ariables

--- app.js
+++ app.js
@@ -22,3 +22,30 @@ var o = new Object(); //similar to action script
 var myArray = new Array();
 
 var anotherObject = {};
+
+var person = {};
+person.name = "sinead";
+person.age = 21;
+person.female = true;
+person.growOld = function (){
+	this.age++;
+}
+
+var anotherperson = {
+
+	name: "Sinead",
+	age: 21,
+	female: true,
+	growOld: function (){
+		this.age++;
+	},
+	friends: new Array(),
+	newFriends: ["tom", "pat"]
+}
+
+for( var i = 0; i < 10; i++){
+	console.log(anotherperson.name + " is " + anotherperson.age);
+	anotherperson.growOld();
+}
+
+
